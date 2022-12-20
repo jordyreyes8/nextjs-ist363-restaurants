950be3d3f7822b4b5f37dce0a335df282ed2b018
jreyes20 <jordyreyes8@gmail.com>
2022-12-19 22:26:23 -0500

    Commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+    return <header className = {styles.footer} >Copyright J. Reyes 2022.</header>
+}
+
+export default Footer
---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'ist-363-restaurants.local',
+        },
+        {
+          protocol: 'https',
+          hostname: 'restaurants.ist363.xyz',
+        },
+      ],
+    },
+  }
